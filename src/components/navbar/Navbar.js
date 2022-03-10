ea540e421232ad2c9b63684d20a41ed16e2acd8f
Abhay5855 <ap3919529@gmail.com>
2022-03-10 15:59:10 +0530

    docs : importing the search icon and setting a state searchOpen to false and when the icon gets clicked change the state to true

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,51 +1,57 @@
 
 
 
-import React from "react";
-import "./navbar.css";
-import { Link } from "react-router-dom";
-
+import React, {useState} from 'react'
+import './navbar.css'
+import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 
 const Navbar = () => {
-  
 
+  const [searchOpen , setSearchOpen] = useState(false);
+
+
+   const searchClick = () => {
+
+         setSearchOpen(true);
+
+   }
   return (
+       
     <nav>
+
       <ul>
-        <Link to="/home" style={{ textDecoration: "none", color: "#fff" }}>
+          <li><a>home</a></li>
           <li>
-            <p>Home</p>
-          </li>
-        </Link>
 
-        <li>
-          {/* <Search /> */}
-        </li>
+          <SearchRoundedIcon style={{ fontSize: 20 }} onClick={searchClick}/>
+            <form>
+        
+              
+              <input className='nav__input' type='text' placeholder='Search..' />
+              <button className='btn__submit' type='submit'></button>
+               
+         
 
-        <Link
-          to="/watch-later"
-          style={{ textDecoration: "none", color: "#fff" }}
-        >
-          <li className="watch_later">
-            <p>Watchlist</p>
-          </li>
-        </Link>
-
-        <li>
-          <Link to='/' style={{textDecoration :'none' , color : '#fff'}}>
-
-          <div >
-            
-            <img className="avatar" src="https://scontent.fnag4-2.fna.fbcdn.net/v/t1.6435-9/68615542_2382368328711243_690095807315050496_n.jpg?stp=dst-jpg_s851x315&_nc_cat=105&ccb=1-5&_nc_sid=da31f3&_nc_ohc=ZNvjyKUckm0AX86anW_&_nc_ht=scontent.fnag4-2.fna&oh=00_AT_O_BhlEeiFPLVy9NFvm17tp7dDoHTy-p73bTOfdIpfvQ&oe=6250A3CE" alt='no-profile'/>
-            
-          </div>
-
-          </Link>
           
-        </li>
+            </form>
+          </li>
+          <li><a>watchlist</a></li>
+        
+          <li>
+              <img className='profile__img' src='https://scontent.fnag4-1.fna.fbcdn.net/v/t1.6435-9/145050096_888591505014385_1337213454963259110_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_ohc=niwTlP7M4P0AX9mpn_c&_nc_ht=scontent.fnag4-1.fna&oh=00_AT_O00ABhHfa4kIvArpJNFNCWRZxX5sCBuY1aM41X6x0BA&oe=624F6F94'  alt='profile'/>
+          </li>
       </ul>
+      
+
     </nav>
-  );
-};
+  )
+}
+
+
+
+
 
 export default Navbar;
+
+
+
